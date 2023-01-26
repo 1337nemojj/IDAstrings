d164b6aaa6c9637210d7d6bc5a514b122a2d9ce2
master <1337nemojj@gmail.com>
2023-01-26 14:52:24 +0300

    colored text

--- sript.py
+++ sript.py
@@ -1,14 +1,31 @@
 #find offset
+import colorama
 data = []
 parsed_data = []
+
+colorama.init()
+RED = colorama.Fore.RED
+GREEN = colorama.Fore.GREEN
+GRAY = colorama.Fore.LIGHTBLACK_EX
+RESET = colorama.Fore.RESET
+YELLOW = colorama.Fore.YELLOW
+#init colors
 
 file = input()
 with open(f"{file}.txt", "r") as file1:
     for line in file1:
         #print(line.strip())
         data.append(line.strip())
-def colored_text(string, color_it):
-    pass
+
+#string ananana/lolol/nama
+def colored_text(str_line, f):
+    start_mark = str_line.find(f)
+    ending_mark = start_mark + len(f)
+    if start_mark > 0:
+        return f"{str_line[:start_mark]}{GREEN}{str_line[start_mark:ending_mark]}{RESET}{str_line[ending_mark:]}"
+    else:
+        return str_line
+
 
 while(True):
     inp = input("[?] Enter starting string pattern: ")
@@ -19,9 +36,12 @@ while(True):
         
         sep = str.split('\t')
         #print(sep[-1])
+        str_line = sep[-1]
         parsed_data.append(sep)
-        if sep[-1][:ending] == inp and f in sep[-1]:
-            print(sep[0], "\t" ,sep[1], "\t" ,sep[-1])
+        if str_line[:ending] == inp and f in str_line:
+            print(sep[0], "\t" ,sep[1], "\t" , colored_text(str_line,f))
             count+=1
+            
+
     print("[+] ALL FOUND: ",count)
 
